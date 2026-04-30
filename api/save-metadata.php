158c8eb5756d4a2082752409aa3154106370ee4b
lehaidang2k9 <146460634+lehaidang2k9@users.noreply.github.com>
2026-04-30 07:50:56 +0700

    Delete api directory

--- api/save-metadata.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-$data = json_decode(file_get_contents('php://input'), true);
-if ($data && isset($_SESSION['user_id'])) {
-    $uuid = bin2hex(random_bytes(16));
-    $stmt = $pdo->prepare("INSERT INTO files (uuid, user_id, original_name, stored_name, mime_type, file_size) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$uuid, $_SESSION['user_id'], $data['name'], $data['url'], $data['type'], $data['size']]);
-}
--- api/upload-token.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'config.php';
-// Vercel Blob yêu cầu trả về token an toàn
-echo json_encode(['token' => getenv('BLOB_READ_WRITE_TOKEN')]);
